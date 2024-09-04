ca495ff7622c8f12aec6aa424415bb8e61a7e741
Miljan Ilic <hello@miljanilic.com>
2024-09-04 23:40:01 +0200

    Add JSQLSelectItemVisitor

---
+++ src/main/java/com/miljanilic/sql/parser/visitor/JSQLSelectItemVisitor.java
@@ -0,0 +1,24 @@
+package com.miljanilic.sql.parser.visitor;
+
+import com.miljanilic.sql.ast.expression.Expression;
+import com.miljanilic.sql.ast.node.Select;
+import com.miljanilic.sql.ast.node.SimpleSelect;
+import net.sf.jsqlparser.expression.ExpressionVisitor;
+import net.sf.jsqlparser.statement.select.SelectItem;
+import net.sf.jsqlparser.statement.select.SelectItemVisitorAdapter;
+
+public class JSQLSelectItemVisitor extends SelectItemVisitorAdapter<Select> {
+    private final ExpressionVisitor<Expression> expressionVisitor;
+
+    public JSQLSelectItemVisitor(ExpressionVisitor<Expression> expressionVisitor) {
+        this.expressionVisitor = expressionVisitor;
+    }
+
+    @Override
+    public <S> Select visit(SelectItem<? extends net.sf.jsqlparser.expression.Expression> item, S context) {
+        return new SimpleSelect(
+                item.getExpression().accept(this.expressionVisitor, context),
+                item.getAlias() != null ? item.getAlias().getName() : null
+        );
+    }
+}
