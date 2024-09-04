e73a5ad4555575921ec9cc311dd341d108a64796
Miljan Ilic <miljan.ilic@fishingbooker.com>
2024-09-04 23:39:07 +0200

    Add JSQLOrderByVisitor

---
+++ src/main/java/com/miljanilic/sql/parser/visitor/JSQLOrderByVisitor.java
@@ -0,0 +1,21 @@
+package com.miljanilic.sql.parser.visitor;
+
+import com.miljanilic.sql.ast.expression.Expression;
+import com.miljanilic.sql.ast.node.OrderBy;
+import com.miljanilic.sql.ast.node.SimpleOrderBy;
+import net.sf.jsqlparser.expression.ExpressionVisitor;
+import net.sf.jsqlparser.statement.select.OrderByElement;
+import net.sf.jsqlparser.statement.select.OrderByVisitor;
+
+public class JSQLOrderByVisitor implements OrderByVisitor<OrderBy> {
+    private final ExpressionVisitor<Expression> expressionVisitor;
+
+    public JSQLOrderByVisitor(ExpressionVisitor<Expression> expressionVisitor) {
+        this.expressionVisitor = expressionVisitor;
+    }
+
+    @Override
+    public <S> OrderBy visit(OrderByElement orderByElement, S context) {
+        return new SimpleOrderBy(orderByElement.getExpression().accept(this.expressionVisitor, context), orderByElement.isAsc());
+    }
+}
