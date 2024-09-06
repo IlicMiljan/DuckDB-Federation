35f1e7bbb8899fdce86d69cfd7cec0c2e8e67b38
Miljan Ilic <hello@miljanilic.com>
2024-09-06 17:52:48 +0200

    Add schema repository

--- src/main/java/com/miljanilic/Main.java
+++ src/main/java/com/miljanilic/Main.java
@@ -2,11 +2,12 @@ package com.miljanilic;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.miljanilic.module.CatalogModule;
 import com.miljanilic.module.SQLModule;
 
 public class Main {
     public static void main(String[] args) {
-        Injector injector = Guice.createInjector(new SQLModule());
+        Injector injector = Guice.createInjector(new SQLModule(), new CatalogModule());
         Application app = injector.getInstance(Application.class);
 
         app.run();
