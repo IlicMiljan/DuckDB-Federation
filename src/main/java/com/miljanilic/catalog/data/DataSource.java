4ec45005ff77ee9bbb4066e5b03c3bc241832a6a
Miljan Ilic <hello@miljanilic.com>
2024-09-06 17:47:41 +0200

    Add schema structure definition

---
+++ src/main/java/com/miljanilic/catalog/data/DataSource.java
@@ -0,0 +1,58 @@
+package com.miljanilic.catalog.data;
+
+public class DataSource {
+    private String jdbcUrl;
+    private String jdbcUser;
+    private String jdbcPassword;
+    private String jdbcDriver;
+
+    public DataSource() {}
+
+    public DataSource(String jdbcUrl, String jdbcUser, String jdbcPassword, String jdbcDriver) {
+        this.jdbcUrl = jdbcUrl;
+        this.jdbcUser = jdbcUser;
+        this.jdbcPassword = jdbcPassword;
+        this.jdbcDriver = jdbcDriver;
+    }
+
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+    }
+
+    public String getJdbcUser() {
+        return jdbcUser;
+    }
+
+    public void setJdbcUser(String jdbcUser) {
+        this.jdbcUser = jdbcUser;
+    }
+
+    public String getJdbcPassword() {
+        return jdbcPassword;
+    }
+
+    public void setJdbcPassword(String jdbcPassword) {
+        this.jdbcPassword = jdbcPassword;
+    }
+
+    public String getJdbcDriver() {
+        return jdbcDriver;
+    }
+
+    public void setJdbcDriver(String jdbcDriver) {
+        this.jdbcDriver = jdbcDriver;
+    }
+
+    @Override
+    public String toString() {
+        return "DataSource{" +
+                "jdbcUrl='" + jdbcUrl + '\'' +
+                ", jdbcUser='" + jdbcUser + '\'' +
+                ", jdbcDriver='" + jdbcDriver + '\'' +
+                '}';
+    }
+}
