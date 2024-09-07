192b285ce6b9c9cccd422f6eae788562624dff3f
Miljan Ilic <hello@miljanilic.com>
2024-09-07 22:53:26 +0200

    Remove unnecessary sql node abstractions

--- src/main/java/com/miljanilic/sql/ast/node/GroupBy.java
+++ src/main/java/com/miljanilic/sql/ast/node/GroupBy.java
@@ -1,8 +1,25 @@
 package com.miljanilic.sql.ast.node;
 
 import com.miljanilic.sql.ast.ASTVisitor;
+import com.miljanilic.sql.ast.expression.Expression;
 
-public abstract class GroupBy extends Node {
+public class GroupBy extends Node {
+    private final Expression expression;
+
+    public GroupBy(Expression expression) {
+        this.expression = expression;
+    }
+
+    public Expression getExpression() {
+        return expression;
+    }
+
+    public <T, S> T accept(ASTVisitor<T, S> visitor, S context) {
+        return visitor.visit(this, context);
+    }
+
     @Override
-    public abstract <T, S> T accept(ASTVisitor<T, S> visitor, S context);
+    public String toString() {
+        return expression.toString();
+    }
 }
--- src/main/java/com/miljanilic/sql/ast/node/OrderBy.java
+++ src/main/java/com/miljanilic/sql/ast/node/OrderBy.java
@@ -1,9 +1,32 @@
 package com.miljanilic.sql.ast.node;
 
 import com.miljanilic.sql.ast.ASTVisitor;
+import com.miljanilic.sql.ast.expression.Expression;
 
-public abstract class OrderBy extends Node {
+public class OrderBy extends Node {
+    private final Expression expression;
+    private final boolean isAscending;
+
+    public OrderBy(Expression expression, boolean isAscending) {
+        this.expression = expression;
+        this.isAscending = isAscending;
+    }
+
+    public Expression getExpression() {
+        return expression;
+    }
+
+    public boolean isAscending() {
+        return isAscending;
+    }
 
     @Override
-    public abstract <T, S> T accept(ASTVisitor<T, S> visitor, S context);
+    public <T, S> T accept(ASTVisitor<T, S> visitor, S context) {
+        return visitor.visit(this, context);
+    }
+
+    @Override
+    public String toString() {
+        return expression + (isAscending ? " ASC" : " DESC");
+    }
 }
