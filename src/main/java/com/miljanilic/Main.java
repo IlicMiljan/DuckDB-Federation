4b7d75505d55a5ddd0ffc1d4cb1ef0769a6dd0f9
Miljan Ilic <hello@miljanilic.com>
2024-09-08 00:49:30 +0200

    Add PlannerModule

--- src/main/java/com/miljanilic/Main.java
+++ src/main/java/com/miljanilic/Main.java
@@ -3,11 +3,12 @@ package com.miljanilic;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.miljanilic.module.CatalogModule;
+import com.miljanilic.module.PlannerModule;
 import com.miljanilic.module.SQLModule;
 
 public class Main {
     public static void main(String[] args) {
-        Injector injector = Guice.createInjector(new SQLModule(), new CatalogModule());
+        Injector injector = Guice.createInjector(new SQLModule(), new CatalogModule(), new PlannerModule());
         Application app = injector.getInstance(Application.class);
 
         app.run();
