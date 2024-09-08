7e18054e24905207ab97dc820c460b54c7e9b538
Miljan Ilic <hello@miljanilic.com>
2024-09-09 01:22:36 +0200

    Add physical statement execution plan decomposition rules

---
+++ src/main/java/com/miljanilic/planner/rule/JoinDecompositionExecutionPlannerRule.java
@@ -0,0 +1,30 @@
+package com.miljanilic.planner.rule;
+
+import com.miljanilic.planner.node.*;
+import java.util.List;
+
+public class JoinDecompositionExecutionPlannerRule extends ExecutionPlannerRuleAdapter {
+    @Override
+    public PlanNode visit(JoinNode node, PlanNode context) {
+        List<PlanNode> children = visitChildren(node.getChildren(), context);
+
+        if (children.getFirst() instanceof RemoteScanNode leftScan && children.getLast() instanceof RemoteScanNode rightScan) {
+            if (leftScan.getSchema().equals(rightScan.getSchema())) {
+                RemoteJoinNode remoteJoinNode = new RemoteJoinNode(
+                        node.getLeft(),
+                        node.getRight(),
+                        node.getJoinType(),
+                        node.getCondition(),
+                        node.getAlgorithm(),
+                        leftScan.getSchema()
+                );
+                remoteJoinNode.setChildren(children);
+
+                return remoteJoinNode;
+            }
+        }
+
+        node.setChildren(children);
+        return node;
+    }
+}
