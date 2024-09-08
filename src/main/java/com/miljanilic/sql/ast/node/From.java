3bab431ed7be41ac19d4b08c39fff62abf6618ee
Miljan Ilic <hello@miljanilic.com>
2024-09-09 01:12:38 +0200

    Add schema to from node

--- src/main/java/com/miljanilic/sql/ast/node/From.java
+++ src/main/java/com/miljanilic/sql/ast/node/From.java
@@ -1,8 +1,19 @@
 package com.miljanilic.sql.ast.node;
 
+import com.miljanilic.catalog.data.Schema;
 import com.miljanilic.sql.ast.ASTVisitor;
 
 public abstract class From extends Node {
+    protected final Schema schema;
+
+    protected From(Schema schema) {
+        this.schema = schema;
+    }
+
+    public Schema getSchema() {
+        return schema;
+    }
+
     @Override
     public abstract <T, S> T accept(ASTVisitor<T, S> visitor, S context);
 }
