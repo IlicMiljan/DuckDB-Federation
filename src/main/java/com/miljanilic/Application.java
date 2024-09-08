1da6c4e2b587c62d6011ed75de452923ed5cd6c5
Miljan Ilic <hello@miljanilic.com>
2024-09-09 01:33:02 +0200

    Update application

--- src/main/java/com/miljanilic/Application.java
+++ src/main/java/com/miljanilic/Application.java
@@ -1,25 +1,37 @@
 package com.miljanilic;
 
 import com.google.inject.Inject;
+import com.miljanilic.catalog.data.Schema;
 import com.miljanilic.planner.*;
+import com.miljanilic.planner.converter.ExecutionPlanStatementConverter;
+import com.miljanilic.planner.filter.ExecutionPlanAggregationNodeFilter;
+import com.miljanilic.planner.filter.ExecutionPlanFilter;
+import com.miljanilic.planner.filter.ExecutionPlanSchemaFilter;
 import com.miljanilic.planner.node.PlanNode;
+import com.miljanilic.planner.visitor.SchemaExtractingExecutionPlanVisitor;
+import com.miljanilic.sql.ast.statement.SelectStatement;
 import com.miljanilic.sql.ast.statement.Statement;
 import com.miljanilic.sql.parser.SQLParser;
 import com.miljanilic.sql.parser.SQLParserException;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Application {
     private final SQLParser sqlParser;
     private final ExecutionPlanner executionPlanner;
+    private final ExecutionPlanStatementConverter executionPlanStatementConverter;
 
     @Inject
     public Application(
             SQLParser sqlParser,
-            ExecutionPlanner executionPlanner
+            ExecutionPlanner executionPlanner,
+            ExecutionPlanStatementConverter executionPlanStatementConverter
     ) {
         this.sqlParser = sqlParser;
         this.executionPlanner = executionPlanner;
+        this.executionPlanStatementConverter = executionPlanStatementConverter;
     }
 
     public void run() {
@@ -35,6 +47,43 @@ public class Application {
 
             PlanNode planRoot = executionPlanner.plan(statement);
             System.out.println(planRoot.explain());
+
+            System.out.println("----------------");
+
+            SelectStatement executionStatement = executionPlanStatementConverter.convert(planRoot);
+            System.out.println(executionStatement);
+
+            System.out.println("----------------");
+
+            ExecutionPlanVisitor<Void, Set<Schema>> schemaExtractor = new SchemaExtractingExecutionPlanVisitor();
+
+            HashSet<Schema> schemas = new HashSet<>();
+            planRoot.accept(schemaExtractor, schemas);
+
+            for (Schema schema : schemas) {
+                System.out.println(schema);
+                System.out.println("----------------");
+            }
+
+            for (Schema schema : schemas) {
+                ExecutionPlanFilter filter = new ExecutionPlanSchemaFilter(schema);
+
+                PlanNode schemaFilteredPlanRoot = filter.filter(planRoot);
+
+                SelectStatement schemaFilteredExecutionStatement = executionPlanStatementConverter.convert(schemaFilteredPlanRoot);
+                System.out.println(schemaFilteredExecutionStatement);
+
+                System.out.println("----------------");
+            }
+
+            ExecutionPlanFilter filter = new ExecutionPlanAggregationNodeFilter();
+
+            PlanNode schemaFilteredPlanRoot = filter.filter(planRoot);
+
+            SelectStatement schemaFilteredExecutionStatement = executionPlanStatementConverter.convert(schemaFilteredPlanRoot);
+            System.out.println(schemaFilteredExecutionStatement);
+
+            System.out.println("----------------");
         } catch (SQLParserException e) {
             System.out.println("Error parsing SQL: " + e.getMessage());
         }
--- src/main/java/com/miljanilic/module/PlannerModule.java
+++ src/main/java/com/miljanilic/module/PlannerModule.java
@@ -1,12 +1,52 @@
 package com.miljanilic.module;
 
 import com.google.inject.AbstractModule;
-import com.miljanilic.planner.ExecutionPlanner;
-import com.miljanilic.planner.LogicQueryExecutionPlanner;
+import com.google.inject.Provides;
+import com.google.inject.TypeLiteral;
+import com.google.inject.multibindings.Multibinder;
+import com.miljanilic.planner.*;
+import com.miljanilic.planner.converter.ExecutionPlanStatementConverter;
+import com.miljanilic.planner.converter.ExecutionPlanVisitorStatementConverter;
+import com.miljanilic.planner.node.PlanNode;
+import com.miljanilic.planner.rule.JoinDecompositionExecutionPlannerRule;
+import com.miljanilic.planner.rule.ProjectDecompositionExecutionPlannerRule;
+import com.miljanilic.planner.rule.TableScanDecompositionExecutionPlannerRule;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class PlannerModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(ExecutionPlanner.class).to(LogicQueryExecutionPlanner.class);
+        bind(ExecutionPlanner.class).to(PhysicalStatementExecutionPlanner.class);
+        bind(ExecutionPlanStatementConverter.class).to(ExecutionPlanVisitorStatementConverter.class);
+
+        Multibinder<ExecutionPlanVisitor<PlanNode, PlanNode>> plannerBinder = Multibinder.newSetBinder(
+                binder(),
+                new TypeLiteral<ExecutionPlanVisitor<PlanNode, PlanNode>>() {}
+        );
+
+        plannerBinder.addBinding().to(TableScanDecompositionExecutionPlannerRule.class);
+        plannerBinder.addBinding().to(JoinDecompositionExecutionPlannerRule.class);
+        plannerBinder.addBinding().to(ProjectDecompositionExecutionPlannerRule.class);
+    }
+
+    @Provides
+    public List<ExecutionPlanVisitor<PlanNode, PlanNode>> provideExecutionPlanVisitors(
+            Set<ExecutionPlanVisitor<PlanNode, PlanNode>> plannerSet
+    ) {
+        return new ArrayList<>(plannerSet);
+    }
+
+    @Provides
+    public PhysicalStatementExecutionPlanner providePhysicalStatementExecutionPlanner(
+            LogicQueryExecutionPlanner logicQueryExecutionPlanner,
+            List<ExecutionPlanVisitor<PlanNode, PlanNode>> planners
+    ) {
+        return new PhysicalStatementExecutionPlanner(
+                logicQueryExecutionPlanner,
+                planners
+        );
     }
 }
