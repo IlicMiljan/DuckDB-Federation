3b0c62210a450c065d3abc111d42e9f866c9b463
Miljan Ilic <hello@miljanilic.com>
2024-09-09 01:12:52 +0200

    Add schema resolving logic to parser

--- src/main/java/com/miljanilic/sql/parser/visitor/JSQLFromItemVisitor.java
+++ src/main/java/com/miljanilic/sql/parser/visitor/JSQLFromItemVisitor.java
@@ -1,16 +1,25 @@
 package com.miljanilic.sql.parser.visitor;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.miljanilic.catalog.SchemaRepository;
 import com.miljanilic.sql.ast.node.From;
 import com.miljanilic.sql.ast.node.Table;
 import net.sf.jsqlparser.statement.select.FromItemVisitorAdapter;
 
 @Singleton
 public class JSQLFromItemVisitor extends FromItemVisitorAdapter<From> {
+    private final SchemaRepository schemaRepository;
+
+    @Inject
+    public JSQLFromItemVisitor(SchemaRepository schemaRepository) {
+        this.schemaRepository = schemaRepository;
+    }
+
     @Override
     public <S> From visit(net.sf.jsqlparser.schema.Table table, S context) {
         return new Table(
-                table.getSchemaName(),
+                this.schemaRepository.getSchema(table.getSchemaName()),
                 table.getName(),
                 table.getAlias() != null ? table.getAlias().getName() : null
         );
