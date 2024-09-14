d73f0c60d515ff5f5f7ec2f502154f6b3483dd67
Miljan Ilic <hello@miljanilic.com>
2024-09-14 18:18:16 +0200

    Add distributed schema SQL execution logic
    Introduce SQLExecutionException, SchemaQueryExecutor, and ConcurrentSchemaQueryExecutor to handle SQL query execution. Update pom.xml to include MySQL and DuckDB dependencies necessary for database connections.

---
+++ src/main/java/com/miljanilic/executor/SchemaQueryExecutor.java
@@ -0,0 +1,39 @@
+package com.miljanilic.executor;
+
+import com.google.inject.Inject;
+import com.miljanilic.catalog.data.Schema;
+import com.miljanilic.sql.ast.statement.SelectStatement;
+import com.miljanilic.sql.deparser.SqlDeParser;
+
+import java.sql.*;
+import java.util.function.BiConsumer;
+
+public class SchemaQueryExecutor {
+    private final SqlDeParser sqlDeParser;
+
+
+    @Inject
+    public SchemaQueryExecutor(SqlDeParser sqlDeParser) {
+        this.sqlDeParser = sqlDeParser;
+    }
+
+    public void execute(Schema schema, SelectStatement query, BiConsumer<SelectStatement, ResultSet> resultSetConsumer) {
+        String sql = sqlDeParser.deparse(query);
+
+        try (
+                Connection conn = DriverManager.getConnection(schema.getDataSource().getJdbcUrl(), schema.getDataSource().getJdbcUser(), schema.getDataSource().getJdbcPassword());
+        ) {
+            Statement statement = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+
+            // TODO: Support other databases.
+            statement.setFetchSize(Integer.MIN_VALUE);  // Enable streaming in MySQL
+
+            try (ResultSet resultSet = statement.executeQuery(sql)) {
+                resultSetConsumer.accept(query, resultSet);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new SQLExecutionException("Error executing SQL query", e);
+        }
+    }
+}
